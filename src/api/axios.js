f8c12989c1624051a2ad28f115cad2f77a55790a
JunYoung Lee <oonju5031@users.noreply.github.com>
2026-02-11 16:09:38 +0900

    chore: axios 의존성 추가, 환경변수 파일 gitignore에 추가
    기타 package.json 내 오탈자 수정

---
+++ src/api/axios.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const url = process.env.REACT_APP_SERVER_URL;
+
+const instance = axios.create({
+  baseURL: url,
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// 요청 인터셉터: 로컬 스토리지에 토큰이 있다면 모든 요청 헤더에 자동으로 포함
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
